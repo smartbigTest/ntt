9a80564d31b0ee252589a2ece60cf39add210616
HwangBeomSeok <bshwang8701@naver.com>
2024-12-05 10:20:02 +0900

    테스트3

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,20 +9,20 @@ module.exports = {
         eth: {
             url: process.env.ETH_MAINNET_RPC_URL,
             ledgerAccounts: [
-                "0x8581B506caf4bB6a28FC7e855aBb059FFA991665"
+                process.env.LEDGER_ACCOUNT
             ] //하드웰렛 이더리움 네트워크 선택 후 레디인 상태에서 해야함 accounts, ledgerAccounts 같이 사용 못함
         },
         sepolia: {
             url: process.env.ETH_SEPOLIA_RPC_URL,
             ledgerAccounts: [
-                "0x8581B506caf4bB6a28FC7e855aBb059FFA991665"
+                process.env.LEDGER_ACCOUNT
             ] //하드웰렛 이더리움 네트워크 선택 후 레디인 상태에서 해야함 accounts, ledgerAccounts 같이 사용 못함
         },
     },
     etherscan: {
         apiKey: {
-            sepolia: "6JJ47F2N6U3CHQNHYAAT3PF887ND3R4IFR",
-            mainnet: "6JJ47F2N6U3CHQNHYAAT3PF887ND3R4IFR"
+            sepolia: process.env.ETH_SCAN_API_KEY,
+            mainnet: process.env.ETH_SCAN_API_KEY
         }
     }
 };
